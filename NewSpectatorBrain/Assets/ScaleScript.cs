63ab2b919e668c3792f9168a7c944dd34d06b62a
Kyle Mas <34122775+kylemas@users.noreply.github.com>
2020-03-02 13:25:04 -0800

    Added scale up/down functionality with voice commands. (#1)

---
+++ NewSpectatorBrain/Assets/ScaleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ScaleScript : MonoBehaviour
+{
+
+    public GameObject scaleObject;
+    private float scaleFactor = 1.5f;
+    private float scaleDownFactor = 0.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void scaleUp()
+    {
+        scaleObject.transform.localScale *= scaleFactor;
+    }
+
+    public void scaleDown()
+    {
+        scaleObject.transform.localScale *= scaleDownFactor;
+    }
+}
